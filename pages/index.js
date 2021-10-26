5f1d1a238f117f87e907682048116212184198b9
Shyam Gupta <shyamsundar.g@kiprosh.com>
2021-10-26 20:49:09 +0530

    PR 1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function HomePage() {
-  return <div>Welcome to github ghost n8n automation!</div>;
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"
+      ></link>
+      <center>
+        <h1>Welcome to github ghost n8n automation</h1>
+      </center>
+    </>
+  );
 }
 
 export default HomePage;
